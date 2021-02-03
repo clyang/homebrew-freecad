d3065832570f9ab51fc6fee4bf910cc8691b1c1c
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2021-02-03 20:28:08 +0100

    Add pythonocc-core.

---
+++ Formula/pythonocc-core.rb
@@ -0,0 +1,20 @@
+class PythonOCCCore < Formula
+  desc "Pythonocc provides 3D modeling and dataexchange features. It is intended to CAD/PDM/PLM and BIM related development."
+  homepage "https://www.pythonocc.org"
+  url "https://github.com/tpaviot/pythonocc-core.git", :using => :git
+  version "7.5.0"
+
+  depends_on "#@tap/opencascade@7.5.0" => :required
+  depends_on "cmake" => :build
+  depends_on "#@tap/python3.9"
+
+  def install
+    mkdir "Build" do
+     system "cmake", '-DOCE_LIB_PATH="' + Formula["#@tap/opencascade@7.5.0"].lib, '-DOCE_INCLUDE_PATH="' + Formula["#@tap/opencascade@7.5.0"].include, *std_cmake_args , ".."
+     system "make", "-j#{ENV.make_jobs}"
+     system "make", "install"
+    end
+  end
+  
+  
+end

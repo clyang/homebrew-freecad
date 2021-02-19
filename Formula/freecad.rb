891affe516fe0cf15afc891b578def0990ac0bbb
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2021-02-19 16:06:04 +0100

    Fix VTK9.0.1 path.

--- Formula/freecad.rb
+++ Formula/freecad.rb
@@ -81,7 +81,7 @@ class Freecad < Formula
       -DCMAKE_BUILD_TYPE=#{build.with?("debug") ? "Debug" : "Release"}
     ]
 
-    args << '-DCMAKE_PREFIX_PATH="' + Formula["#@tap/qt5152"].opt_prefix + "/lib/cmake;" + Formula["#@tap/nglib"].opt_prefix + "/Contents/Resources;" + Formula["#@tap/vtk@8.2.0"].opt_prefix + "/lib/cmake;" + Formula["#@tap/opencascade@7.5.0"].opt_prefix + "/lib/cmake;"+ Formula["#@tap/med-file"].opt_prefix + "/share/cmake/;" + Formula["#@tap/shiboken2"].opt_prefix + "/lib/cmake;" + Formula["#@tap/pyside2"].opt_prefix+ "/lib/cmake;" + Formula["#@tap/coin@4.0.0"].opt_prefix+ "/lib/cmake;" + Formula["#@tap/boost@1.75.0"].opt_prefix+ "/lib/cmake;" + Formula["#@tap/boost-python3@1.75.0"].opt_prefix+ "/lib/cmake;"
+    args << '-DCMAKE_PREFIX_PATH="' + Formula["#@tap/qt5152"].opt_prefix + "/lib/cmake;" + Formula["#@tap/nglib"].opt_prefix + "/Contents/Resources;" + Formula["#@tap/vtk@9.0.1"].opt_prefix + "/lib/cmake;" + Formula["#@tap/opencascade@7.5.0"].opt_prefix + "/lib/cmake;"+ Formula["#@tap/med-file"].opt_prefix + "/share/cmake/;" + Formula["#@tap/shiboken2"].opt_prefix + "/lib/cmake;" + Formula["#@tap/pyside2"].opt_prefix+ "/lib/cmake;" + Formula["#@tap/coin@4.0.0"].opt_prefix+ "/lib/cmake;" + Formula["#@tap/boost@1.75.0"].opt_prefix+ "/lib/cmake;" + Formula["#@tap/boost-python3@1.75.0"].opt_prefix+ "/lib/cmake;"
 
     args << "-DFREECAD_CREATE_MAC_APP=1" if build.with? "macos-app"
     args << "-DBUILD_CLOUD=1" if build.with? "cloud"

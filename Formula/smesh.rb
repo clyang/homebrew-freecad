267aa8250534f847aaeb32a34c4549abb7efd409
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2021-02-26 21:01:25 +0100

    Add smesh formula.

---
+++ Formula/smesh.rb
@@ -0,0 +1,21 @@
+class Smesh < Formula
+  desc "A stand-alone library of the mesh framework from the Salome Platform."
+  homepage "https://github.com/trelau/SMESH"
+  url "https://github.com/trelau/SMESH.git", :using => :git
+  version "0.1.0"
+
+  depends_on "cmake" => :build
+
+  def install
+    
+    sytsem "python prepare.py"
+    
+    mkdir "Build" do
+     system "cmake",  *args , ".."
+     system "make", "-j#{ENV.make_jobs}"
+     system "make", "install"
+    end
+  end
+  
+  
+end
